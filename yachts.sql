7ba1b83464604b69f1c8bce80ad7108bcb56fda8
Elizaveta Mironovich <elizavetamironovich@yandex.ru>
2018-05-09 01:15:21 +0300

    yachts insertions

---
+++ yachts.sql
@@ -0,0 +1,15 @@
+insert into yachts (id, name, class_rang) values (3071, 'Fayth', 1);
+insert into yachts (id, name, class_rang) values (4660, 'Philippe', 1);
+insert into yachts (id, name, class_rang) values (9649, 'Josephine', 3);
+insert into yachts (id, name, class_rang) values (9291, 'Clifford', 2);
+insert into yachts (id, name, class_rang) values (9778, 'Sybila', 5);
+insert into yachts (id, name, class_rang) values (1039, 'Aubrie', 5);
+insert into yachts (id, name, class_rang) values (2596, 'Kaile', 3);
+insert into yachts (id, name, class_rang) values (8780, 'Corabelle', 2);
+insert into yachts (id, name, class_rang) values (3937, 'Trevor', 1);
+insert into yachts (id, name, class_rang) values (5097, 'Elmore', 3);
+insert into yachts (id, name, class_rang) values (4318, 'Loree', 2);
+insert into yachts (id, name, class_rang) values (9683, 'Giffie', 3);
+insert into yachts (id, name, class_rang) values (7535, 'Charla', 4);
+insert into yachts (id, name, class_rang) values (5181, 'Jody', 3);
+insert into yachts (id, name, class_rang) values (8422, 'Mercedes', 2);
