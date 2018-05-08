8a0fa115117c0f3d1914b6e9f2b6a9e9797c0ba4
Elizaveta Mironovich <elizavetamironovich@yandex.ru>
2018-05-09 00:29:09 +0300

    classes inserts

---
+++ classes.sql
@@ -0,0 +1,5 @@
+insert into classes (rang, size, displacement, cost_per_month, cost_per_day, cost_per_day_overdue) values (1, 17, 40, 3000000, 67200, 95000);
+insert into classes (rang, size, displacement, cost_per_month, cost_per_day, cost_per_day_overdue) values (2, 20, 41, 2500000, 52784, 678241);
+insert into classes (rang, size, displacement, cost_per_month, cost_per_day, cost_per_day_overdue) values (3, 20, 38, 2000000, 41230, 517426);
+insert into classes (rang, size, displacement, cost_per_month, cost_per_day, cost_per_day_overdue) values (4, 20, 44, 1400000, 39440, 448965.7);
+insert into classes (rang, size, displacement, cost_per_month, cost_per_day, cost_per_day_overdue) values (5, 21, 42, 1000000, 37091, 434516.88);
