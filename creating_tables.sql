829f2b8beccc20ef6699ec50509deaedb996fd45
Elizaveta Mironovich <elizavetamironovich@yandex.ru>
2018-05-09 00:54:39 +0300

    changed clients' id to varchar from int

--- creating_tables.sql
+++ creating_tables.sql
@@ -17,11 +17,11 @@ CREATE TABLE yachts(
 )
 
 CREATE TABLE clients(
-	id INT PRIMARY KEY,
+	id VARCHAR(20) PRIMARY KEY,
 	name VARCHAR(100) NOT NULL,
 	address VARCHAR(200) NOT NULL,
 	phone_number NUMERIC(11) NOT NULL,
-	bank_account NUMERIC(20),
+	bank_account VARCHAR(30),
 	times_overdue_payment INT DEFAULT 0,
 	times_damaged INT DEFAULT 0
 )
@@ -29,7 +29,7 @@ CREATE TABLE clients(
 CREATE TABLE contracts(
 	number INT IDENTITY(1,1) PRIMARY KEY,
 	yacht_id INT REFERENCES yachts(id),
-	client_id INT REFERENCES clients(id),
+	client_id VARCHAR(20) REFERENCES clients(id),
 	openning_date DATE NOT NULL,
 	expected_closing_date DATE NOT NULL,
 	payment_scheme CHAR(13),
