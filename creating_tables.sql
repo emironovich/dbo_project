7692481264dd946db2e92adea3a447a2421eb0d4
Elizaveta Mironovich <elizavetamironovich@yandex.ru>
2018-05-09 00:09:37 +0300

    fixing creation

--- creating_tables.sql
+++ creating_tables.sql
@@ -1,8 +1,8 @@
 CREATE TABLE classes (
 	rang INT NOT NULL,
-	name VARCHAR PRIMARY KEY NOT NULL,
+	name VARCHAR(70) PRIMARY KEY NOT NULL,
 	size REAL,
-	displacment REAL,
+	displacement REAL,
 	cost_per_month MONEY NOT NULL,
 	cost_per_day MONEY NOT NULL,
 	cost_per_day_overdue MONEY NOT NULL
@@ -10,17 +10,17 @@ CREATE TABLE classes (
 
 CREATE TABLE yachts(
 	id INT PRIMARY KEY,
-	name VARCHAR NOT NULL,
-	class_name VARCHAR REFERENCES classes(name), --может лучше ввести номер для классов
-	placment CHAR(42) NOT NULL,
+	name VARCHAR(70) NOT NULL,
+	class_name VARCHAR(70) REFERENCES classes(name), --может лучше ввести номер для классов
+	placment VARCHAR(42) NOT NULL,
 	condition VARCHAR DEFAULT 'in order',
 	last_check DATE
 )
 
 CREATE TABLE clients(
 	id INT PRIMARY KEY,
-	name CHAR(42) NOT NULL,
-	adress CHAR(42) NOT NULL,
+	name VARCHAR(100) NOT NULL,
+	address VARCHAR(200) NOT NULL,
 	phone_number NUMERIC(11) NOT NULL,
 	bank_account NUMERIC(20),
 	times_overdue_payment INT DEFAULT 0,
