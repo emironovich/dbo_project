c76505ca06e9d7d04bc13c15099891ebf17d2a29
Elizaveta Mironovich <elizavetamironovich@yandex.ru>
2018-05-08 20:24:07 +0300

    new tables scheme

--- creating_tables.sql
+++ creating_tables.sql
@@ -1,17 +1,19 @@
 CREATE TABLE classes (
 	rang INT NOT NULL,
-	name CHAR(42) PRIMARY KEY NOT NULL,
+	name VARCHAR PRIMARY KEY NOT NULL,
 	size REAL,
 	displacment REAL,
+	cost_per_month MONEY NOT NULL,
 	cost_per_day MONEY NOT NULL,
-	cost_per_month MONEY
+	cost_per_day_overdue MONEY NOT NULL
 )
 
 CREATE TABLE yachts(
 	id INT PRIMARY KEY,
-	class_name CHAR(42) REFERENCES classes(name), --может лучше ввести номер для классов
+	name VARCHAR NOT NULL,
+	class_name VARCHAR REFERENCES classes(name), --может лучше ввести номер для классов
 	placment CHAR(42) NOT NULL,
-	condition CHAR(42) DEFAULT 'in order',
+	condition VARCHAR DEFAULT 'in order',
 	last_check DATE
 )
 
@@ -21,22 +23,18 @@ CREATE TABLE clients(
 	adress CHAR(42) NOT NULL,
 	phone_number NUMERIC(11) NOT NULL,
 	bank_account NUMERIC(20),
-	next_payment_due DATE,
-	times_overdue INT DEFAULT 0,
+	times_overdue_payment INT DEFAULT 0,
 	times_damaged INT DEFAULT 0
 )
 
 CREATE TABLE contracts(
-	number INT IDENTITY(1,1) PRIMARY KEY, -- может какой-нибудь вычисляемый столбец
+	number INT IDENTITY(1,1) PRIMARY KEY,
 	yacht_id INT REFERENCES yachts(id),
 	client_id INT REFERENCES clients(id),
-	beginning DATE NOT NULL,
-	ending DATE NOT NULL,  
+	openning_date DATE NOT NULL,
+	expected_closing_date DATE NOT NULL,
 	payment_scheme CHAR(13),
-	CHECK(payment_scheme IN ('upfront', 'half upfront', 'monthly'))
-)
-
-CREATE TABLE payments(
-	contract_number INT REFERENCES contracts(number),
-	date DATE NOT NULL
+	status CHAR(7),
+	paid_until DATE,
+	CHECK((payment_scheme IN ('upfront', 'half upfront', 'monthly')) AND (STATUS IN ('open', 'closed')))
 )
