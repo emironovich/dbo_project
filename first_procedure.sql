c29cb5d259c9db75f42929e7579f7cb332c33ecb
Elizaveta Mironovich <elizavetamironovich@yandex.ru>
2018-05-09 15:40:00 +0300

    frist procedure for updating cost per month

---
+++ first_procedure.sql
@@ -0,0 +1,23 @@
+CREATE PROCEDURE increase_cost_per_month_by_percent (@percent INT) AS
+BEGIN
+	DECLARE my_cur CURSOR FOR 
+	SELECT cost_per_month
+	FROM classes
+	FOR UPDATE OF cost_per_month;
+
+	OPEN my_cur;
+	DECLARE @cur_cost MONEY;
+
+	FETCH NEXT FROM my_cur 
+	INTO @cur_cost;
+
+	WHILE (@@FETCH_STATUS = 0)
+	BEGIN
+		UPDATE classes SET cost_per_month = @cur_cost * (1 + @percent * 0.01) WHERE CURRENT OF my_cur;
+		FETCH NEXT FROM my_cur 
+		INTO @cur_cost;
+	END
+
+	CLOSE my_cur;
+	DEALLOCATE my_cur;
+END
